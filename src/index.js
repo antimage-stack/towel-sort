6bf83def97efd44623a9cced798a77d20921a5aa
antimage-stack <vanua.osipovporg@gmail.com>
2021-02-16 15:27:32 +0300

    docs: finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if ( !matrix || matrix.length == 0 ) return []
+
+  let array = []
+  
+  for ( let i = 1; i <= matrix.length - 1; i+= 2 ) {
+    matrix[i].reverse()
+  }
+
+  for ( let item of matrix ) {
+    array = array.concat(item)
+  }
+  return array
 }
